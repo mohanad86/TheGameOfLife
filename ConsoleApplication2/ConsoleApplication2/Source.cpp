30d8c466085c7f4795a565ba5662a1ecf5e9c468
mohanad86 <capmohanad@hotmail.com>
2017-01-07 01:04:35 +0200

    Deleting folders

--- ConsoleApplication2/ConsoleApplication2/Source.cpp
+++
@@ -1,7 +0,0 @@
-int *p_allocated = (int*)malloc(sizeof(int) * n);
-int *p_copy = p_allocated;
-// programmer should use p_copy, not with p_allocated!
-// e.g.:
-while (n--) { *p_copy++ = n; }
-...
-free(p_allocated);
