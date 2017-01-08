fefb1d726d12ccf1d85e8d8c69d38fc7af833736
mohanad86 <capmohanad@hotmail.com>
2017-01-08 14:13:50 +0200

    Setting grid

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
-
 int main()
-{	
-	
+{
+
+
 	//starting with the window
-	sf::RenderWindow window(sf::VideoMode(600, 600), "Welcome to Mohanad Game of life");
+	sf::RenderWindow window(sf::VideoMode(400, 400), "Welcome to Mohanad Game of life");
+	sf::RectangleShape;
 	while (window.isOpen())
 	{
+		window.clear(sf::Color::White);
 		sf::Event event;
-		while (window.waitEvent(event))
+		while (window.pollEvent(event))
 		{
 			switch (event.type)
 			{
@@ -20,9 +21,30 @@ int main()
 			}break;
 			case sf::Event::Resized:
 			{
+				sf::FloatRect visibleArea(30, 30, event.size.width, event.size.height);
+				window.setView(sf::View(visibleArea));
 			}break;
 			}
+
 		}
-		window.display();
-	}
-}
+		
+		sf::Vector2i Grid(30, 30);
+		sf::Texture setGrid;
+		sf::Sprite setGridSprite;
+		setGridSprite.setTexture(setGrid);
+		setGridSprite.setColor(sf::Color::Blue);
+		for (int i = 0; i < Grid.x; i++)
+		{
+			for (int j = 0; j < Grid.y; j++)
+			{
+				setGridSprite.setPosition(j * 32, i * 32);
+				setGridSprite.setTextureRect(sf::IntRect(Grid.x * 20, Grid.y * 20, 20, 20));
+				window.draw(setGridSprite);
+
+
+					}
+				}
+
+				window.display();
+			}
+		}
