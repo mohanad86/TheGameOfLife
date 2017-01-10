0375ee84953224eb596a8790797533eea78f05c2
mohanad86 <capmohanad@hotmail.com>
2017-01-10 02:49:24 +0200

    Create the vector

--- Grid.h
+++ Grid.h
@@ -1,22 +1,30 @@
 #pragma once
 #define Grid_header
 #include <SFML/Graphics.hpp>
-
+#include "Cell.h"
+#include <vector>
 
 using namespace sf;
+using namespace std;
+
 
 class Grid
 {
 public:
 	void Grid::draw(RenderWindow & window);
 	Grid();
-	Grid(int , int );
+	Grid(int, int);
+	std::vector<Cell>Cell;
+	bool single_cell_width;
+	bool single_cell_height;
+
 
 
 	
 	
 	
 private:
+
 	RectangleShape grid;
 };
 
