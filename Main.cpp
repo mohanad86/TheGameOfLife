61e17ab341d4862617309e76c8172b5972b77309
mohanad86 <capmohanad@hotmail.com>
2017-01-11 17:10:22 +0200

    Addnig Main.cpp and starting with to make the window visible

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+	//starting with the window 
+	sf::Window window(sf::VideoMode(800, 600), "Welcome to Mohanad Game Of Life");
+	window.setFramerateLimit(60);
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.setActive();
+		window.display();
+		}
+
+}
+
