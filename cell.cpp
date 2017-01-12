af517bc4194db93f625c07c2ae8f9aec287240a2
mohanad86 <capmohanad@hotmail.com>
2017-01-13 00:25:32 +0200

    Setting the (target) which is the shape and set the color and position for it

--- cell.cpp
+++ cell.cpp
@@ -1,12 +1,24 @@
 #include "cell.h"
+//this is the shape which is called the target
+RectangleShape  cell::target;
 
 
-
-cell::cell()
+void cell::Setup(int gridWidth, int gridHeight)
 {
+	target = RectangleShape(sf::Vector2f(gridWidth, gridHeight));
+	Setcolor(Color::White);
+	target.setOutlineThickness(1.0f);
+	target.setOutlineColor(sf::Color::Red);
+
 }
 
+void cell::Setcolor(Color color) {
+	target.setFillColor(color);
 
-cell::~cell()
-{
+}
+
+void cell::Setposition(int x, int y) {
+
+	target.setPosition(x, y);
+
 }
--- cell.h
+++ cell.h
@@ -1,8 +1,21 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
+//here to make easy
+using namespace sf;
+
 class cell
 {
 public:
-	cell();
-	~cell();
+	static RectangleShape target;
+
+
+public:
+	//setup the cell
+	static void Setup(int gridWidth, int gridHeight);
+	//set the colors
+	static void Setcolor(Color color);
+	//set the position
+	static void Setposition(int x, int y);
 };
 
